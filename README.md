99d789a8df2761830685b13aeb8cd58b4c79c8ee
fishermanswharff <fishermanswharff@github.com>
2015-01-21 21:58:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
+
+# GA WDI Rails Repo Template Generator
+
+To use this generator, clone this repo to your local machine. 
+Run `rails-api new <app_name> --database=postgresql -T -m path/to/this/file/template.rb`
+Do *NOT* change the folders and files of this repo, or I can't vouch for your results. `template.rb` uses the `/files` directory as an addition to the `source_paths`.
---
+++ template.rb
@@ -0,0 +1,83 @@
+
+# To use this app template:
+# rails-api new <app_name> --database=postgresql -T -m path/to/this/file/template.rb
+
+def source_paths
+  Array(super) + 
+    [File.join(File.expand_path(File.dirname(__FILE__)),'files')]
+end
+
+insert_into_file 'Gemfile', "\nruby '2.2.0'", after: "source 'https://rubygems.org'\n"
+gem 'newrelic_rpm'
+gem 'rack-cors'
+gem 'active_model_serializers', github: 'rails-api/active_model_serializers'
+gem 'nokogiri'
+
+# add gems for dev & test env
+gem_group :development, :test do
+  gem "rspec-rails", "~> 3.1.0"
+  gem "pry-byebug"
+  gem "pry-rails"
+  gem "factory_girl_rails", "~> 4.0"
+  gem "faker"
+  gem "codeclimate-test-reporter", require: nil
+end
+
+# add gems for production env
+gem_group :production do
+  gem "unicorn"
+  gem "rails_12factor"
+  gem "rails_stdout_logging"
+  gem "rails_serve_static_assets"
+end
+
+gsub_file "Gemfile", /[#].*/,''
+gsub_file "Gemfile", /[\n]+/,"\n"
+
+in_root do
+  run "rspec --init"
+  template '.travis.yml'
+  template '.ruby-version'
+  template 'LICENSE'
+  template 'README.md'
+  remove_file 'README.rdoc'
+end
+
+inside 'app' do
+  inside 'serializers' do
+  end
+end
+
+after_bundle do
+  run "rake db:create"
+  run "rake db:migrate"
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
+
+# you may need to make more specific changes to files that 
+# aren't supported directly by the API. Thor provides two 
+# methods to give you this control: insert_into_file and gsub_file.
+
+=begin
+gsub_file "Gemfile", /^gem\s+["']sqlite3["'].*$/,''
+gsub_file "Gemfile", /^gem\s+["']turbolinks["'].*$/,''
+insert_into_file 'Gemfile', "\nruby '2.1.0'", 
+ after: "source 'https://rubygems.org'\n"
+inside 'config' do
+  insert_into_file 'environment.rb', "$stdout.sync = true\n"
+  before: "# Load the rails application"
+end
+=end
+
+
+
+
+
+
+
+
+
+
+
